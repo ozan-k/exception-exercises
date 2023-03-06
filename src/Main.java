cac15d38ef7293409b2685b4a5692e76898ff74d
ozan-k <ozan.kah@gmail.com>
2023-03-06 13:01:58 +0100

    4-finally

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,32 @@ public class Main {
     boolean continueLoop = true; // determines if more input is needed
 
     while (continueLoop) {
+      Integer numerator, denominator, quotient = null;
+
       try { // read two numbers and calculate quotient
         System.out.print("Please enter an integer numerator: ");
-        int numerator = scanner.nextInt();
+        numerator = scanner.nextInt();
 
         System.out.print("Please enter an integer denominator: ");
-        int denominator = scanner.nextInt();
+        denominator = scanner.nextInt();
 
-        int quotient = quotient(numerator, denominator);
+        quotient = quotient(numerator, denominator);
         System.out.printf("%nResult: %d / %d = %d%n", numerator, 
 denominator, quotient);
 
         continueLoop = false;
       } catch (InputMismatchException e) {
-        e.printStackTrace();
         scanner.nextLine(); // discard input so we can try again
+        e.printStackTrace();
         System.out.printf("You must enter integers. Please try again.%n");
       } catch (ArithmeticException e) {
         e.printStackTrace();
         System.out.printf("Cannot divide by 0. Please try again.%n");
+      } finally {
+        if (quotient != null)
+          System.out.printf("OK: Successful division!%n");
+        else
+          System.out.printf("ERROR: Unsuccessful division!%n");
       }
     }
   }
