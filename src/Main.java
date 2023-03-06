b5dd5ef0bc630371eeedf41e4a28c430ae8c0092
ozan-k <ozan.kah@gmail.com>
2023-03-06 13:06:44 +0100

    6-chained-exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
 
   public static void main(String[] args) {
-    String fileName = "bohemian-rhapsody.txt";
-
-    try (FileWriter writer = new FileWriter(fileName)) {
-      writer.write("Is this the real life?\n");
-      writer.write("Is this just fantasy?\n");
-      writer.write("Caught in a landslide\n");
-      writer.write("No escape from reality\n");
-    } catch(IOException e){
-      System.out.println("Could not write to file! :(");
+    try {
+      method1();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  public static void method1() throws Exception {
+    try {
+      method2();
+    } catch (Exception e) {
+      throw new Exception("Exception thrown in method1()", e);
+    }
+  }
+
+  public static void method2() throws Exception {
+    try {
+      method3();
+    } catch (Exception e) {
+      throw new Exception("Exception thrown in method2()", e);
+    }
+  }
+
+  public static void method3() throws Exception {
+    throw new Exception("Exception thrown in method3()");
   }
 
 }
