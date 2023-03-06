8bacab16d0f724c8d37e6e2cb07fd0bacf2715eb
ozan-k <ozan.kah@gmail.com>
2023-03-06 13:09:24 +0100

    7-custom-exception

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,14 @@ public class Main {
 
   public static void main(String[] args) {
     try {
-      method1();
-    } catch (Exception e) {
+      throwMyException();
+    } catch (MyException e) {
       e.printStackTrace();
     }
   }
 
-  public static void method1() throws Exception {
-    try {
-      method2();
-    } catch (Exception e) {
-      throw new Exception("Exception thrown in method1()", e);
-    }
-  }
-
-  public static void method2() throws Exception {
-    try {
-      method3();
-    } catch (Exception e) {
-      throw new Exception("Exception thrown in method2()", e);
-    }
-  }
-
-  public static void method3() throws Exception {
-    throw new Exception("Exception thrown in method3()");
+  public static void throwMyException() throws MyException {
+    throw new MyException();
   }
 
 }
