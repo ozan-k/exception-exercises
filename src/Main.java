e2840b2f8eb1b93e51e76d8459c9c1d2616cc722
ozan-k <ozan.kah@gmail.com>
2023-03-06 13:04:52 +0100

    5-try-resources

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,20 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    boolean continueLoop = true; // determines if more input is needed
+    String fileName = "bohemian-rhapsody.txt";
 
-    while (continueLoop) {
-      Integer numerator, denominator, quotient = null;
-
-      try { // read two numbers and calculate quotient
-        System.out.print("Please enter an integer numerator: ");
-        numerator = scanner.nextInt();
-
-        System.out.print("Please enter an integer denominator: ");
-        denominator = scanner.nextInt();
-
-        quotient = quotient(numerator, denominator);
-        System.out.printf("%nResult: %d / %d = %d%n", numerator, 
-denominator, quotient);
-
-        continueLoop = false;
-      } catch (InputMismatchException e) {
-        scanner.nextLine(); // discard input so we can try again
-        e.printStackTrace();
-        System.out.printf("You must enter integers. Please try again.%n");
-      } catch (ArithmeticException e) {
-        e.printStackTrace();
-        System.out.printf("Cannot divide by 0. Please try again.%n");
-      } finally {
-        if (quotient != null)
-          System.out.printf("OK: Successful division!%n");
-        else
-          System.out.printf("ERROR: Unsuccessful division!%n");
-      }
+    try (FileWriter writer = new FileWriter(fileName)) {
+      writer.write("Is this the real life?\n");
+      writer.write("Is this just fantasy?\n");
+      writer.write("Caught in a landslide\n");
+      writer.write("No escape from reality\n");
+    } catch(IOException e){
+      System.out.println("Could not write to file! :(");
     }
   }
 
-  public static int quotient(int numerator, int denominator) {
-    return numerator / denominator;
-  }
 }
 
